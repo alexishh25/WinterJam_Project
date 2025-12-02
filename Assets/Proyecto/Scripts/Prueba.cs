63c1dae8891a057ba05c81691e188bc828b8c14b
AlexisHH <147951736+AlexisHerencia25@users.noreply.github.com>
2025-12-02 17:16:47 -0500

    Cambios chicos

--- Assets/Proyecto/Scripts/Prueba.cs
+++ Assets/Proyecto/Scripts/Prueba.cs
@@ -6,19 +6,15 @@ public class Prueba : MonoBehaviour
 
     private Rigidbody2D rb2d;
     private Sprite miSprite;
-    private SpriteRenderer sr;
     private float thrust = 1f;
 
     void Awake()
     {
-        sr = gameObject.AddComponent<SpriteRenderer>();
         rb2d = gameObject.AddComponent<Rigidbody2D>();
     }
 
     void Start()
     {
-        sr.color = new Color(0.9f, 0.9f, 0.5f, 1.0f);
-        sr.sprite = miSprite;
         transform.position = new Vector3(0.0f, -2.0f, 0.0f);
     }
 
